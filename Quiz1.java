3432f31fb6b2822c8338f6c30cff03f22b091f0c
Alodia C. Alueta <alodia.c.alueta@gmail.com>
2025-04-30 12:38:55 +0000

    Quiz(OddEven) April 30, 2025

--- Quiz1.java
+++ Quiz1.java
@@ -3,7 +3,7 @@ class Quiz1 {
 public static void main(String[]args){
     Scanner input = new Scanner (System.in);
 
-    System.out.print("Enter #: ");
+    System.out.print("Enter number: ");
     int num = input.nextInt();
 
     if (num %2==0){
